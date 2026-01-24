dcd80bd6cd4b512f9f2175c5332847f534253a15
Ofek Pintok <OfekPintok@Gmail.com>
2026-01-24 09:34:27 +0200

    chore(infrastructure): configure global Coil with memory caching strategy
    - Implemented `ImageLoaderFactory` in Application class.
    - Configured Memory Cache (25% RAM) to prevent OOM.
    - Configured Disk Cache strategy (even though unused for assets currently).

--- app/src/main/java/com/ofekpintok/shutterfly/canvas/core/ShutterflyCanvasApplication.kt
+++ app/src/main/java/com/ofekpintok/shutterfly/canvas/core/ShutterflyCanvasApplication.kt
@@ -1,11 +1,15 @@
 package com.ofekpintok.shutterfly.canvas.core
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import com.ofekpintok.shutterfly.canvas.core.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class ShutterflyCanvasApplication : Application() {
+class ShutterflyCanvasApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         setupDi()
@@ -17,4 +21,20 @@ class ShutterflyCanvasApplication : Application() {
             modules(appModules)
         }
     }
+
+    override fun newImageLoader(): ImageLoader =
+        ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .crossfade(true)
+            .build()
 }
